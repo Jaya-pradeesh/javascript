0c96727a97821645126f97190284534cbc831e6b
Jaya-pradeesh <147582777+Jaya-pradeesh@users.noreply.github.com>
2024-04-04 09:48:20 +0530

    Add files via upload

---
+++ imageform/sample.js
@@ -0,0 +1,40 @@
+//  function aboutU(name, age, city ){
+//     var result=" ";
+//     result += "Hii Iam"+" "+name+" "+", Iam "+" "+" my native is "+" "+city;
+//     return result;
+// }
+
+// console.log(aboutU("Pradeesh","22","Chennai"))
+
+// //nested array
+
+// var sample=[['chennai' ,28],['mumbai', 69]]
+// console.log(sample)
+
+// //access array element using index element
+
+// var outarray=[1,2,3,4,5,6,7]
+// var resultarray=outarray[4]
+// console.log(resultarray)
+
+// //modify array wiht index element
+
+// outarray[4]=88;
+//  console.log(outarray)
+
+// function resusableCode(){
+//     console.log("hello world")
+
+  
+// }
+// console.log(resusableCode('print'))
+
+// function addition(a,b){
+//     console.log(a+b)
+// }
+// addition(2,5)
+
+let name = prompt('enter your name?')
+
+
+
